66a079b055f4a0289762899a04677e1d2ba1f38b
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-07-11 23:42:43 +0530

    Adding Stack

--- com/Bridgelabz/Main.java
+++ com/Bridgelabz/Main.java
@@ -1,5 +1,14 @@
 package com.Bridgelabz;
 
-public class Main {
-
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Stack and Queues.");
+		Stack stack = new Stack();	
+		stack.push(70);
+		stack.push(30);
+		stack.push(56);		
+		stack.print();
+	}
 }
