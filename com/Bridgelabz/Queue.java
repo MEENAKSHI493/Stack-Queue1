d5b5d53427a5025e46469af354dd4847f8eaa7f2
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-07-11 23:55:26 +0530

    Stack and Queue Peek Pop

---
+++ com/Bridgelabz/Queue.java
@@ -0,0 +1,57 @@
+package com.Bridgelabz;
+
+public class Queue 
+{
+	Node front;
+	Node rear;
+	int length;
+	class Node
+	{
+		int data;
+		Node next;
+		public Node(int data) 
+		{
+			this.data = data;
+			this.next = null;
+		}
+	}
+		//add data into the Queue
+	public void enQueue(int data) 
+	{
+		if (front == null) 
+		{
+			rear = new Node(data);
+			front = rear;
+		}
+		else 
+		{
+			rear.next = new Node(data);
+			rear = rear.next;
+		}
+		length ++;
+	}
+      //Print method 
+	public void print() 
+	{
+		Node temp = front;
+		while(temp != null) 
+		{
+			System.out.print(temp.data + "-> ");
+			temp = temp.next;
+		}
+
+	}
+	//Delete Data From Queue
+		public void deQueue() 
+		{
+			if (front != null) 
+			{
+				int temp = front.data;
+				front = front.next;
+				length--;
+				return;
+			}			
+	}
+
+
+}
